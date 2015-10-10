9a3efd6451b8ceacba056cf9c136e071918f4e1a
Greg Smolyn <greg@smolyn.org>
2015-10-10 10:24:17 -0700

    package updates (rolled up projectile changes, sorry)

--- init.el
+++ init.el
@@ -96,6 +96,30 @@
 (require 'org)
 (setq org-log-done t)
 
+;;
+;; jsx/react
+;;
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+(flycheck-define-checker jsxhint-checker
+  "A JSX syntax and style checker based on JSXHint."
+
+  :command ("jsxhint" source)
+  :error-patterns
+  ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+  :modes (web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (equal web-mode-content-type "jsx")
+              ;; enable flycheck
+              (flycheck-select-checker 'jsxhint-checker)
+              (flycheck-mode))))
+(eval-after-load "web-mode" '(setq web-mode-tag-auto-close-style 0))
+
 ;; visual/UI
 
 (load-theme 'wombat t)
