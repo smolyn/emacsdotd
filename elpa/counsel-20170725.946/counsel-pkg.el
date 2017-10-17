86d4afb26bc8c87b60beb375f6a57d07df1c66f9
Greg <gsmolyn@salesforce.com>
2017-10-17 13:59:34 -0700

    upgrade

--- elpa/counsel-20170725.946/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20170725.946" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "112c5ba0df7deea11b3e91b5db3990d693eb5b72" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
