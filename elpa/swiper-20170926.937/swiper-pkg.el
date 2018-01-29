62cc5cd462b842196127d917a83214faf5cbe318
Greg <gsmolyn@salesforce.com>
2018-01-29 10:13:14 -0800

    updated to latest packages

--- elpa/swiper-20170926.937/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20170926.937" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "4a66bb6afd981923f923483168c03a9d89c61191" :url "https://github.com/abo-abo/swiper" :keywords '("matching"))
