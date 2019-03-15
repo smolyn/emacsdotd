2eaa6382844b553a28733a43e16f1f0caf24fdfd
Greg Smolyn <gsmolyn@salesforce.com>
2019-03-15 10:14:55 -0700

    updated emacs packages march 2019

---
+++ elpa/git-commit-20190102.107/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190102.107" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "e5a2a0ac24e303606f40a7de40c37c8164ef6913" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
