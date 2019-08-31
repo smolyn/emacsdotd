667d92172a6a65f1cba0778d02e319e53fe78cff
Greg Smolyn <gsmolyn@salesforce.com>
2019-08-30 19:44:21 -0700

    aug 2019 updates

--- elpa/swiper-20190627.1944/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20190627.1944" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.11.0")) :commit "de8842e44a478e590988fcddf3a296e021de4730" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
