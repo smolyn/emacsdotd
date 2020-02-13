6a7658e99c2c30eeccb8400c23e7b240db82a41f
Greg Smolyn <gsmolyn@salesforce.com>
2020-02-13 12:30:11 -0800

    enabled and init tide mode

--- init.el
+++ init.el
@@ -265,6 +265,30 @@
 ; fzf
                                         ;(use-package "fzf" :init (setenv "FZF_DEFAULT_COMMAND" "fd --type f"))
 
+
+;; typescript/tide
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
