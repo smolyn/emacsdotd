1d53e792f61a1ec7c10e0d1306faf3b409f3367b
Greg Smolyn <gsmolyn@salesforce.com>
2021-05-05 14:16:05 -0700

    update packages may 2021

--- elpa/counsel-20210109.1641/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/smolyn/.emacs.d/elpa/counsel-20210109.1641/counsel.el  -*- no-byte-compile: t -*-
-(define-package "counsel" "20210109.1641" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "cbce9ce71429c98c67bd76ef15d049ecced042f7" :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :keywords '("convenience" "matching" "tools") :url "https://github.com/abo-abo/swiper")
