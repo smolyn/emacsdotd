7526d0675375e77d132b3fa91d2a0843c29d35d3
Greg Smolyn <gsmolyn@salesforce.com>
2021-06-29 12:46:13 -0700

    updated packages

---
+++ elpa/magit-section-20210615.1036/magit-section-pkg.el
@@ -0,0 +1,13 @@
+(define-package "magit-section" "20210615.1036" "Sections for read-only buffers"
+  '((emacs "25.1")
+    (dash "20210330"))
+  :commit "3a39338a4382de13972adb55c528a8ec68e7ab21" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/projectile-20210629.528/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from projectile.el  -*- no-byte-compile: t -*-
+(define-package "projectile" "20210629.528" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :commit "e8a3f11df57583ad089a4f00d652c80566ffeb09" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :keywords '("project" "convenience") :url "https://github.com/bbatsov/projectile")
